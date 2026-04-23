757484ec0058567cfb73d10405892e3903bc1a40
Erubiel Huerta <erhu2729@colorado.edu>
2026-04-22 19:02:55 -0600

    fixed argc bug

--- dfs.c
+++ dfs.c
@@ -17,7 +17,7 @@ int get_filenames_from_directory(char *directory, char *filename, char filenames
 int send_packet(int sockfd, char *method, char *filename, int chunk, int size);
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "Usage: %s <directory> <port>\n", argv[0]);
         exit(1);
     }
